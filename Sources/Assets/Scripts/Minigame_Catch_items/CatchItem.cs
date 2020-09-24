8876b23a31664363a96e292e241225d2f0fa3710
StamplerJ <jann.luellmann@web.de>
2020-09-24 15:14:00 +0100

    Implemented Catch Item Minigame

---
+++ Sources/Assets/Scripts/Minigame_Catch_items/CatchItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchItem : MonoBehaviour
+{
+    public float fallSpeed = 6f;
+
+    public int faith;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.down * (fallSpeed * Time.deltaTime));
+
+        if (transform.position.y < -6f)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Sources/Assets/Scripts/Minigame_Catch_items/ItemSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public GameObject leftWall, rightWall;
+    public List<GameObject> prefabs;
+
+    public float spawnInterval = 1f;
+    
+    private int spawnCounter;
+    private float spawnTimer;
+    
+    
+    void Start()
+    {
+            
+    }
+
+    void Update()
+    {
+        spawnTimer += Time.deltaTime;
+
+        if (spawnTimer >= spawnInterval)
+        {
+            GameObject prefab = prefabs[Random.Range(0, prefabs.Count)];
+            float xPosition = Random.Range(leftWall.transform.position.x + leftWall.transform.localScale.x / 2f, rightWall.transform.position.x - rightWall.transform.localScale.x);
+            GameObject item = Instantiate(prefab, new Vector3(xPosition, 6f, 0f), Quaternion.identity);
+
+            spawnCounter++;
+            spawnTimer -= spawnInterval;
+        }
+    }
+}
