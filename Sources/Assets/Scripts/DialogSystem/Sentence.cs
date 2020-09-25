67cb281cd602535af6284e8056c03c4ff4010b1a
StamplerJ <jann.luellmann@web.de>
2020-09-25 16:51:20 +0100

    Dialog System started

---
+++ Sources/Assets/Scripts/DialogSystem/Sentence.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sentence
+{
+    public Image image;
+    public string text;
+    
+    
+}
--- Sources/Assets/Scripts/SceneChanger.cs
+++ Sources/Assets/Scripts/SceneChanger.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+    private const string ParentScene = "ParentScene";
+    
     public Animator transition;
     public float transitionTime = 1f;
     
@@ -21,16 +22,37 @@ public class SceneChanger : MonoBehaviour
             _instance = this;
         }
         
-        currentScene = SceneManager.GetActiveScene().name;
+        // Get current scene except ParentScene
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            if (!SceneManager.GetSceneAt(i).name.Equals(ParentScene))
+            {
+                currentScene = SceneManager.GetSceneAt(i).name;
+            }
+        }
         lastScenes = new Stack<string>();
     }
 
     public void LoadScene(string sceneName)
     {
-        lastScenes.Push(currentScene);
-        currentScene = sceneName;
+        if (currentScene.Length == 0)
+            return;
 
-       StartCoroutine(Load());
+        lastScenes.Push(currentScene);
+        StartCoroutine(Load(sceneName));
+    }
+
+    IEnumerator Load(string newScene)
+    {
+        transition.SetTrigger("FadeIn");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.UnloadSceneAsync(currentScene);
+        SceneManager.LoadScene(newScene, LoadSceneMode.Additive);
+        currentScene = newScene;
+
+        transition.SetTrigger("FadeOut");
     }
 
     public void LoadLastScene()
@@ -38,19 +60,9 @@ public class SceneChanger : MonoBehaviour
         if (lastScenes.Count <= 0)
             return;
         
-        currentScene = lastScenes.Pop();
-        StartCoroutine(Load());
+        StartCoroutine(Load(lastScenes.Pop()));
     }
 
-    IEnumerator Load()
-    {
-        transition.SetTrigger("StartFading");
-
-        yield return new WaitForSeconds(transitionTime);
-        
-        SceneManager.LoadScene(currentScene);
-    }
-    
     private static SceneChanger _instance;
 
     public static SceneChanger Instance { get { return _instance; } }
