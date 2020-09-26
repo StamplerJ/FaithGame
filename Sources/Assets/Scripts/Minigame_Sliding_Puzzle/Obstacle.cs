f2c62c3e0b89dc64346e2f71b2f45528ba4c9d94
StamplerJ <jann.luellmann@web.de>
2020-09-26 04:31:40 +0100

    Started the sliding puzzle

---
+++ Sources/Assets/Scripts/Minigame_Sliding_Puzzle/Obstacle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public bool moveHorizontal;
+    public bool moveVertical;
+
+    private Rigidbody2D rigidbody;
+    
+    private Vector3 startPosition;
+    private Vector3 offset;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+
+        if (!moveHorizontal)
+            rigidbody.constraints |= RigidbodyConstraints2D.FreezePositionX;
+        
+        if (!moveVertical)
+            rigidbody.constraints |= RigidbodyConstraints2D.FreezePositionY;
+    }
+
+    private void OnMouseDown()
+    {
+        startPosition = transform.position;
+        offset = startPosition - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+
+        Vector3 position = transform.position;
+        position = new Vector3(
+            (moveHorizontal ? curPosition.x : position.x),
+            (moveVertical   ? curPosition.y : position.y),
+            0f);
+
+        if (tag.Equals("Player"))
+        {
+            position = position.Clamp(5f, 5.5f, 0f);
+        }
+        else
+        {
+            position = position.Clamp(5f, 5f, 0f);
+        }
+        
+        rigidbody.MovePosition(position.SnapOffset(new Vector3(0f, 0.5f, 0f)));
+    }
+}
---
+++ Sources/Assets/Scripts/Utils/Vector3Extensions.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Credits: https://answers.unity.com/questions/589983/using-mathfround-for-a-vector3.html
+public static class Vector3Extensions
+{
+    /// <summary>
+    /// Snap Vector3 to nearest grid position
+    /// </summary>
+    /// <param name="vector3">Sloppy position</param>
+    /// <param name="gridSize">Grid size</param>
+    /// <returns>Snapped position</returns>
+    public static Vector3 Snap(this Vector3 vector3, float gridSize = 1.0f)
+    {
+        return new Vector3(
+            Mathf.Round(vector3.x / gridSize) * gridSize,
+            Mathf.Round(vector3.y / gridSize) * gridSize,
+            Mathf.Round(vector3.z / gridSize) * gridSize);
+    }
+    /// <summary>
+    /// Snap Vector3 to nearest grid position with offset
+    /// </summary>
+    /// <param name="vector3">Sloppy position</param>
+    /// <param name="gridSize">Grid size</param>
+    /// <returns>Snapped position</returns>
+    public static Vector3 SnapOffset(this Vector3 vector3, Vector3 offset, float gridSize = 1.0f)
+    {
+        Vector3 snapped = vector3 + offset;
+        snapped = new Vector3(
+            Mathf.Round(snapped.x / gridSize) * gridSize,
+            Mathf.Round(snapped.y / gridSize) * gridSize,
+            Mathf.Round(snapped.z / gridSize) * gridSize);
+        return snapped - offset;
+    }
+
+    public static Vector3 Clamp(this Vector3 vector3, float xRange, float yRange, float zRange)
+    {
+        return new Vector3(
+            Mathf.Clamp(vector3.x, -xRange, xRange),
+            Mathf.Clamp(vector3.y, -yRange, yRange),
+            Mathf.Clamp(vector3.z, -zRange, zRange)
+            );
+    }
+}
