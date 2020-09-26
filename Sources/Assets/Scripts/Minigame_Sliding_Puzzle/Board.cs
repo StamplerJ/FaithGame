7e752d400643624ee5f683fd38d7439dc1348921
StamplerJ <jann.luellmann@web.de>
2020-09-26 16:55:10 +0100

    Removed 2D physics

---
+++ Sources/Assets/Scripts/Minigame_Sliding_Puzzle/Board.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width, height;
+    private Cell[][] fields;
+    
+    void Start()
+    {
+        
+    }
+    
+    private static Board _instance;
+    public static Board Instance { get { return _instance; } }
+    
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        } else {
+            _instance = this;
+        }
+    }
+}
--- Sources/Assets/Scripts/Minigame_Sliding_Puzzle/Obstacle.cs
+++ Sources/Assets/Scripts/Minigame_Sliding_Puzzle/Obstacle.cs
@@ -8,26 +8,19 @@ public class Obstacle : MonoBehaviour
     public bool moveHorizontal;
     public bool moveVertical;
 
-    private Rigidbody2D rigidbody;
-    
     private Vector3 startPosition;
     private Vector3 offset;
 
     private void Awake()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
 
-        if (!moveHorizontal)
-            rigidbody.constraints |= RigidbodyConstraints2D.FreezePositionX;
-        
-        if (!moveVertical)
-            rigidbody.constraints |= RigidbodyConstraints2D.FreezePositionY;
     }
 
     private void OnMouseDown()
     {
         startPosition = transform.position;
-        offset = startPosition - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
+        offset = new Vector3(0f, 0.5f, 0f); 
+        //offset = startPosition - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
     }
 
     private void OnMouseDrag()
@@ -49,7 +42,7 @@ public class Obstacle : MonoBehaviour
         {
             position = position.Clamp(5f, 5f, 0f);
         }
-        
-        rigidbody.MovePosition(position.SnapOffset(new Vector3(0f, 0.5f, 0f)));
+
+        transform.position = position.SnapOffset(offset);
     }
 }
