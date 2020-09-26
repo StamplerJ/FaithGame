f334b86f04394b21b8fc3ae2d163e3fb987e301b
Lyzzerd <53278886+Lyzzerd@users.noreply.github.com>
2020-09-26 13:22:35 +0100

    CharacterSelection

---
+++ Sources/Assets/Scripts/CharacterSelection/CharRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharRotation : MonoBehaviour
+{
+    public GameObject plane;
+    private Transform targetCamera;
+    private Vector3 targetPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetCamera = Camera.main.transform;
+        targetPosition = new Vector3(0f,0f,0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        plane.transform.LookAt(targetCamera);
+    }
+}
